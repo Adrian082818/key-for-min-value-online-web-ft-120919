be4aa2edc3a90253515bd079d7edf7a6b3a80793
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-16 03:10:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,7 @@ def key_for_min_value(name_hash)
   min_key = nil 
   min_val = nil 
   name_hash.each do |name, num|
-  if min_val == nil 
-    else 
-      num < min_val
+  if min_val == nil || num < min_val
       min_val = num
       min_key = name 
 end 
